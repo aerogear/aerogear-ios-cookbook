77fdf7b6bff3a77f45315dcca6755ede3a0b5fa3
corinnekrych <corinnekrych@gmail.com>
2013-07-17 18:06:49 +0200

    rename folders

--- AeroDoc/AeroDocTests/AeroDocTests.h
+++ AeroDoc/AeroDocTests/AeroDocTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2013 AeroGear. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
-
-@interface AeroDocTests : SenTestCase
-
-@end
+//#import <SenTestingKit/SenTestingKit.h>
+//
+//@interface AeroDocTests : SenTestCase
+//
+//@end
---
+++ AeroDoc/AeroDocTests/AeroDocTests.m
@@ -0,0 +1,32 @@
+//
+//  AeroDocTests.m
+//  AeroDocTests
+//
+//  Created by Corinne Krych on 6/17/13.
+//  Copyright (c) 2013 AeroGear. All rights reserved.
+//
+
+//#import "AeroDocTests.h"
+//
+//@implementation AeroDocTests
+//
+//- (void)setUp
+//{
+//    [super setUp];
+//    
+//    // Set-up code here.
+//}
+//
+//- (void)tearDown
+//{
+//    // Tear-down code here.
+//    
+//    [super tearDown];
+//}
+//
+//- (void)testExample
+//{
+//    STFail(@"Unit tests are not implemented yet in AeroDocTests");
+//}
+//
+//@end
