94426216f692c8140c5925438d185887096ba144
corinnekrych <corinnekrych@gmail.com>
2013-12-24 11:52:37 +0100

    add Google Drive demo

---
+++ GoogleDrive/GoogleDrive/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GoogleDrive
+//
+//  Created by Corinne Krych on 12/19/13.
+//  Copyright (c) 2013 AeroGear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AGAppDelegate class]));
+    }
+}
---
+++ GoogleDrive/Podfile
@@ -0,0 +1,4 @@
+xcodeproj 'GoogleDrive.xcodeproj'
+platform :ios, '5.0'
+         
+pod 'AeroGear', :git => 'https://github.com/corinnekrych/aerogear-ios.git', :branch => 'AGIOS-138'
