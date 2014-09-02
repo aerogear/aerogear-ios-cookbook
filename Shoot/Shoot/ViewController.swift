b987a9c4dfc62568a8b78b8c0bbb4f15a7afcd1e
corinnekrych <corinnekrych@gmail.com>
2014-09-02 15:10:39 +0200

    Add camera roll

--- Shoot/Shoot/ViewController.swift
+++ Shoot/Shoot/ViewController.swift
@@ -1,16 +1,36 @@
-//
-//  ViewController.swift
-//  Shoot
-//
-//  Created by Corinne Krych on 02/09/14.
-//  Copyright (c) 2014 AeroGear. All rights reserved.
-//
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright Red Hat, Inc., and individual contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 
 import UIKit
+import MobileCoreServices
+import AssetsLibrary
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
 UIActionSheetDelegate, UIAlertViewDelegate {
-                            
+
+    var newMedia: Bool = true
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,7 +40,59 @@ UIActionSheetDelegate, UIAlertViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK - Toolbar Actions
+    
+    @IBAction func useCamera(sender: UIBarButtonItem) {
+        if (UIImagePickerController.isSourceTypeAvailable(.Camera)) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .Camera
+            imagePicker.mediaTypes = NSArray(object: kUTTypeImage)
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated:true, completion:nil)
+            newMedia = true
+        }
+        
+    }
+    
+    @IBAction func useCameraRoll(sender: UIBarButtonItem) {
+        if (UIImagePickerController.isSourceTypeAvailable(.SavedPhotosAlbum)) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self;
+            imagePicker.sourceType = .PhotoLibrary
+            imagePicker.mediaTypes = NSArray(object: kUTTypeImage)
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated:true, completion:nil)
+            newMedia = false
+        }
+    }
+    
+    // MARK - UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
+        self.dismissViewControllerAnimated(true, completion:nil)
+        var image: UIImage = info[UIImagePickerControllerOriginalImage] as UIImage
+        if (newMedia == true) {
+            UIImageWriteToSavedPhotosAlbum(image, self, Selector("image:didFinishSavingWithError:contextInfo:"), nil)
+            self.imageView.accessibilityIdentifier = "Untitled.jpg";
+        } else {
+            var imageURL:NSURL = info[UIImagePickerControllerReferenceURL] as NSURL
+            var assetslibrary = ALAssetsLibrary()
+            assetslibrary.assetForURL(imageURL, resultBlock: {
+                (asset: ALAsset!) in
+                if asset != nil {
+                    var assetRep: ALAssetRepresentation = asset.defaultRepresentation()
+                    self.imageView.accessibilityIdentifier = assetRep.filename()
+                }
+            }, failureBlock: {
+                (error: NSError!) in
+                println("Error\(error)")
+            }
+            )
+        }
+        self.imageView.image = image;
+    }
 
 }
 
