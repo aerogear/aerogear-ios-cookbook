0acd14edda727de69e0109b8ffabd75d57e7fd4c
corinnekrych <corinnekrych@gmail.com>
2014-09-02 10:04:12 +0200

    Storyboad layout

---
+++ Shoot/Shoot/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Shoot
+//
+//  Created by Corinne Krych on 02/09/14.
+//  Copyright (c) 2014 AeroGear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
+UIActionSheetDelegate, UIAlertViewDelegate {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
