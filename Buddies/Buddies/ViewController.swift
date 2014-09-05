0b61a015df4155717c1acf71e8ac0a331ec0c267
corinnekrych <corinnekrych@gmail.com>
2014-09-05 18:08:24 +0200

    Bring Buddies to beta7 level + latest http lib

--- Buddies/Buddies/ViewController.swift
+++ Buddies/Buddies/ViewController.swift
@@ -16,7 +16,6 @@
 */
 
 import UIKit
-//import AeroGearHttp
 
 class MasterViewController: UITableViewController {
 
@@ -25,7 +24,7 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var http = Session(url: "http://igtests-cvasilak.rhcloud.com/rest/team/developers", sessionConfig: NSURLSessionConfiguration.defaultSessionConfiguration())
+        var http = Http(url: "http://igtests-cvasilak.rhcloud.com/rest/team/developers", sessionConfig: NSURLSessionConfiguration.defaultSessionConfiguration())
         
         http.GET(success: {(response: AnyObject?) in
             if (response != nil) {
@@ -48,8 +47,8 @@ class MasterViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
         let developer = data[indexPath.row]
-        cell.textLabel.text = developer.name
-        cell.detailTextLabel.text = developer.twitter
+        cell.textLabel?.text = developer.name
+        cell.detailTextLabel?.text = developer.twitter
         cell.tag = indexPath.row
         
         let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
@@ -58,7 +57,7 @@ class MasterViewController: UITableViewController {
             
             dispatch_async(dispatch_get_main_queue(), {
                 if cell.tag == indexPath.row {
-                    cell.imageView.image = UIImage(data: imageData)
+                    cell.imageView?.image = UIImage(data: imageData)
                     cell.setNeedsLayout()
                 }
             })
@@ -66,7 +65,7 @@ class MasterViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(tableView: UITableView!, didDeselectRowAtIndexPath indexPath: NSIndexPath!) {
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
         let developer = data[indexPath.row]
         let twitterURL: NSURL = NSURL(string:"http://twitter.com/\(developer.twitter)")
         UIApplication.sharedApplication().openURL(twitterURL)
