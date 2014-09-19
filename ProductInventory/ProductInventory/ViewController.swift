51b60f38ef5d8f097b3f0ea28d2cee8563bcbee3
corinnekrych <corinnekrych@gmail.com>
2014-09-19 17:18:53 +0200

    Keycloak sample on access and refresh tokens, still waiting for revoke implementation

---
+++ ProductInventory/ProductInventory/ViewController.swift
@@ -0,0 +1,75 @@
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright Red Hat, Inc., and individual contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    var oauth2Module:OAuth2Module
+    
+    required init(coder aDecoder: NSCoder) {
+        var config = Config(base: "http://192.168.0.37:8080/auth",
+            authzEndpoint: "realms/product-inventory/tokens/login",
+            redirectURL: "org.aerogear.KeycloakDemo://oauth2Callback",
+            accessTokenEndpoint: "realms/product-inventory/tokens/access/codes",
+            clientId: "product-inventory",
+            refreshTokenEndpoint: "realms/product-inventory/tokens/refresh",
+            revokeTokenEndpoint: "rest/revoke")
+        var session = UntrustedMemoryOAuth2Session(accountId: "MyAccount")
+        self.oauth2Module = OAuth2Module(config: config, accountId: "MyAccount", session: session)
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func requestAccessToken(sender: UIButton) {
+        println("---> Request access token")
+        self.oauth2Module.requestAccessSuccess({(obj) in
+            println("AccessToken \(obj!)")
+        }, failure: { error in
+            println("Access Error: \(error)")
+        })
+    }
+
+    @IBAction func revokeTokens(sender: AnyObject) {
+        println("---> Revoke tokens")
+        // TODO AGIOS-206 waiting for KEYCLOAK-312
+        self.oauth2Module.revokeAccessSuccess({(obj) in
+            println("RevokeToken \(obj!)")
+            }, failure: { error in
+                println("Revoke Error: \(error)")
+        })
+    }
+    
+    @IBAction func requestRefreshToken(sender: UIButton) {       
+        println("---> Request refresh token")
+        self.oauth2Module.refreshAccessTokenSuccess({(obj) in
+            println("RefreshToken \(obj!)")
+            }, failure: { error in
+                println("Refresh Error: \(error)")
+        })
+    }
+}
+
