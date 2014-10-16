046494f33c8dd3e1856a23c3fc0176769d5bf725
Christos Vasilakis <cvasilak@gmail.com>
2014-10-16 19:55:23 +0300

    sync http-lib / oauth2-lib to latest version

--- libs/AeroGearOAuth2/DateUtils.swift
+++ libs/AeroGearOAuth2/DateUtils.swift
@@ -17,9 +17,20 @@
 
 import Foundation
 
+/**
+Handy extensions to NSDate
+*/
 extension NSDate
 {
-    convenience init(dateString:String) {
+    
+    /**
+    Initialize a date object using the given string
+    
+    :param: dateString the string that will be used to instantiate the date object. The string is expected to be in the format 'yyyy-MM-dd hh:mm:ss a'
+    
+    :returns: the NSDate object
+    */
+    public convenience init(dateString:String) {
         let dateStringFormatter = NSDateFormatter()
         dateStringFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss a"
         let d = dateStringFormatter.dateFromString(dateString)
@@ -29,7 +40,14 @@ extension NSDate
             self.init()
         }
     }
-    func toString() -> String {
+    
+    
+    /**
+    Returns a string of the date object using the format 'yyyy-MM-dd hh:mm:ss a'
+    
+    :returns: a formatted string object
+    */
+    public func toString() -> String {
         let dateStringFormatter = NSDateFormatter()
         dateStringFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss a"
         return dateStringFormatter.stringFromDate(self)
