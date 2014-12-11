d040e7f45041ce4ff6b734817e976067b4a732a5
corinne krych <corinnekrych@gmail.com>
2014-12-11 11:22:05 +0100

    Main flow

--- SharedShoot/SharedShoot/ViewController.swift
+++ SharedShoot/SharedShoot/ViewController.swift
@@ -19,8 +19,10 @@ import UIKit
 class ViewController: UIViewController {
     var userInfo: OpenIDClaim?
     var keycloakHttp = Http()
-    var facebookHttp = Http()
-    var googleHttp = Http()
+    var images: [UIImage] = []
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -43,21 +45,6 @@ class ViewController: UIViewController {
             })
         }
     }
-
-    @IBAction func loginAsFacebook(sender: AnyObject) {
-        let facebookConfig = FacebookConfig(
-            clientId: "YYY",
-            clientSecret: "XXX",
-            scopes:["photo_upload, publish_actions"],
-            isOpenIDConnect: true)
-        var oauth2Module = AccountManager.addFacebookAccount(facebookConfig)
-        self.facebookHttp.authzModule = oauth2Module
-        oauth2Module.login {(accessToken: AnyObject?, claims: OpenIDClaim?, error: NSError?) in
-            println(">>> Facebook: \n\(claims)")
-            self.userInfo = claims
-            self.performSegueWithIdentifier("seeImages:", sender: self)
-        }
-    }
     
     @IBAction func loginAsKeycloak(sender: AnyObject) {
         let keycloakConfig = KeycloakConfig(
@@ -68,57 +55,46 @@ class ViewController: UIViewController {
         var oauth2Module = AccountManager.addKeycloakAccount(keycloakConfig)
         self.keycloakHttp.authzModule = oauth2Module
         oauth2Module.login {(accessToken: AnyObject?, claims: OpenIDClaim?, error: NSError?) in
-            println(">>> Keycloak: \n\(claims)")
             self.userInfo = claims
-            //self.performSegueWithIdentifier("seeImages:", sender: self)
+            //self.nameLabel.text = "Hello \(claims?.name)"
             
-            
+            // Get the list of photos
             self.keycloakHttp.GET("http://localhost:8080/shoot/rest/photos", parameters: nil, completionHandler: { (response: AnyObject?, error: NSError?) -> Void in
                 if error != nil {
-                    println("ERROR::\(error)")
+                    println("Oops something must have being wrong. Check your URL. Is your Keycloak server running? \n\(error)")
                 }
-                var image = response![0] as [String: AnyObject]
-                var fileId: AnyObject? = image["filename"]
-                println("Get list of photos::\(response) \n \(fileId)")
-                
-
-                var fileManager = NSFileManager.defaultManager()
-                
-                var path  = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
-                fileManager.createDirectoryAtPath(path, withIntermediateDirectories: true, attributes: nil, error: nil)
-                
-                var finalDestination = path.stringByAppendingPathComponent("Untitledssss.jpg")
-                
-                self.keycloakHttp.download("http://localhost:8080/shoot/rest/photos/images/\(fileId!)",
-                    progress: { (bytesWritten, totalBytesWritten, totalBytesExpectedToWrite)  in
-                        println("bytesWritten: \(bytesWritten), totalBytesWritten: \(totalBytesWritten), totalBytesExpectedToWrite: \(totalBytesExpectedToWrite)")
-                    }, completionHandler: { (response, error) in
-                        var httpResponse = response as NSHTTPURLResponse
-                        
-                        var image = UIImage(contentsOfFile: finalDestination)
-                        //httpResponse
-                        println("Photo::\(response) \(image)")
-
+                var files = response as [AnyObject]
+                files.map({ (file: AnyObject) -> () in
+                    let image = file as? [String: AnyObject]
+                    if let image = image {
+                        let fileId = image["filename"] as String
+                        // Find the path where the photo will be downloaded
+                        let fileManager = NSFileManager.defaultManager()
+                        let path  = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+                        fileManager.createDirectoryAtPath(path, withIntermediateDirectories: true, attributes: nil, error: nil)
+                        let finalDestination = path.stringByAppendingPathComponent(fileId)
+                        // Download the photo one by one
+                        self.keycloakHttp.download("http://localhost:8080/shoot/rest/photos/images/\(fileId)",
+                            progress: { (bytesWritten, totalBytesWritten, totalBytesExpectedToWrite)  in
+                                println("bytesWritten: \(bytesWritten), totalBytesWritten: \(totalBytesWritten), totalBytesExpectedToWrite: \(totalBytesExpectedToWrite)")
+                            }, completionHandler: { (response, error) in
+                                if let fileBase64EncodedContent =  NSString(contentsOfFile: finalDestination, encoding: NSUTF8StringEncoding, error: nil) {
+                                    if let data = NSData(base64EncodedString: fileBase64EncodedContent, options: NSDataBase64DecodingOptions.allZeros) {
+                                        if let image = UIImage(data: data) {
+                                            self.images.append(image)
+                                        }
+                                    }
+                                }
+                                // Sucessfull login, all photo downloaded let's display first one
+                                if  self.images.count > 0 && self.imageView.image == nil {
+                                    self.imageView.image = self.images[0]
+                                }
+                        })
+                    }
                 })
-
             })
-
-
         }
     }
     
-    @IBAction func loginAsGoogle(sender: AnyObject) {
-        let googleConfig = GoogleConfig(
-            clientId: "302356789040-eums187utfllgetv6kmbems0pm3mfhgl.apps.googleusercontent.com",
-            scopes:["https://www.googleapis.com/auth/drive"],
-            isOpenIDConnect: true)
-        var oauth2Module = AccountManager.addGoogleAccount(googleConfig)
-        self.googleHttp.authzModule = oauth2Module
-        oauth2Module.login {(accessToken: AnyObject?, claims: OpenIDClaim?, error: NSError?) in
-            println(">>> Google:\n\(claims)")
-            self.userInfo = claims
-            self.performSegueWithIdentifier("seeImages:", sender: self)
-        }
-    }
 }
 
