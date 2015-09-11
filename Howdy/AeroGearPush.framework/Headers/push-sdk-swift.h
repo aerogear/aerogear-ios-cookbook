743059c84dcba0a3a594f73ebbe7e38330a3c543
corinne krych <corinnekrych@gmail.com>
2015-09-11 10:01:30 +0200

    initial commit

---
+++ Howdy/AeroGearPush.framework/Headers/push-sdk-swift.h
@@ -0,0 +1,28 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for push-sdk-swift.
+FOUNDATION_EXPORT double push_sdk_swiftVersionNumber;
+
+//! Project version string for push-sdk-swift.
+FOUNDATION_EXPORT const unsigned char push_sdk_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <push_sdk_swift/PublicHeader.h>
+
+
