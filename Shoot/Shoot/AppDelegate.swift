a3af4e8532b70103430769d605d797078d0eccb3
corinne krych <corinnekrych@gmail.com>
2016-11-29 18:56:53 +0100

    Shoot is Swift3

--- Shoot/Shoot/AppDelegate.swift
+++ Shoot/Shoot/AppDelegate.swift
@@ -24,52 +24,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions:[NSObject : AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         prepareDefaultSettings()
         return true
     }
     
-    private func prepareDefaultSettings() {
-        let userDefaults = NSUserDefaults.standardUserDefaults()
+    fileprivate func prepareDefaultSettings() {
+        let userDefaults = UserDefaults.standard
         
-        let clear = userDefaults.boolForKey("clearShootKeychain")
+        let clear = userDefaults.bool(forKey: "clearShootKeychain")
         if (clear) {
             print("clearing Keychain")
             let kc = KeychainWrap()
-            kc.resetKeychain()
+            _ = kc.resetKeychain()
         }
         // default values
-        userDefaults.registerDefaults(["key_url" : ""])
+        userDefaults.register(defaults: ["key_url" : ""])
 
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-         NSNotificationCenter.defaultCenter().postNotificationName(AGAppDidBecomeActiveNotification, object:nil)
+         NotificationCenter.default.post(name: NSNotification.Name(rawValue: AGAppDidBecomeActiveNotification), object:nil)
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
-        let notification = NSNotification(name: AGAppLaunchedWithURLNotification, object:nil, userInfo:[UIApplicationLaunchOptionsURLKey:url])
-        NSNotificationCenter.defaultCenter().postNotification(notification)
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let notification = Notification(name: Notification.Name(rawValue: AGAppLaunchedWithURLNotification), object:nil, userInfo:[UIApplicationLaunchOptionsKey.url:url])
+        NotificationCenter.default.post(notification)
         return true
     }
 }
